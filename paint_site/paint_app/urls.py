1667f17ff3271c3fc6fb4174c69f5995d5854ccc
jlotto8 <jlotto8@gmail.com>
2022-12-08 22:04:19 -0500

    with post method and html

--- paint_site/paint_app/urls.py
+++ paint_site/paint_app/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path
 
 from paint_app.views import ColorPickerView
--- paint_site/paint_app/views.py
+++ paint_site/paint_app/views.py
@@ -1,24 +1,47 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
 from django.views import View
 
 from paint_app.forms import ColorPickerForm
-
-
 # Create your views here.
+
 class ColorPickerView(View):
-    def get(self, request):
-        """Present the color picker form and color the user"""
-        form = ColorPickerForm()
+    def get (self,request):
+        color_form = ColorPickerForm()
+       
+        red_value = 255
+        blue_value = 255
+        green_value = 255
 
         context = {
-            'form': form,
-            'red': 255,
-            'green': 255,
-            'blue': 255,
+            'form': color_form,
+            'red': red_value,
+            'green': green_value,
+            'blue': blue_value,
         }
 
-        return render(request, 'paint.html', context=context)
+        return render(
+            request = request,
+            template_name = 'color_picker.html',
+            context= context,
+            )
+
+    def post(self, request):
+        color_form = ColorPickerForm()
+        
+        red_value = int(request.POST['red_amount'])
+        blue_value = int(request.POST['blue_amount'])
+        green_value = int(request.POST['green_amount'])
+
+        context = {
+                'form': color_form,
+                'red': red_value,
+                'green': green_value,
+                'blue': blue_value,
+
+            }
+
+        return render(
+                request = request,
+                template_name = 'color_picker.html',
+                context = context,
+                )
